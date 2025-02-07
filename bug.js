72d955f7f6481e2315f6bd5f650b2a2d74f01f38
BugHunter-X <askzenai@gmail.com>
2025-02-07 03:40:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+//This code will work fine, but if you use a different port and use that same port in another app the app will not run
